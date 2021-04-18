5da64a8ea71acc35f06b621b0ac77595fc682681
Your Name <you@example.com>
2021-04-18 16:43:35 +0900

    apg4b/ex16

---
+++ apg4b/ex16/main.cpp
@@ -0,0 +1,25 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main()
+{
+  vector<int> A(5);
+  rep(i, 5)
+  {
+    cin >> A[i];
+  }
+
+  string ans = "NO";
+
+  rep(i, A.size())
+  {
+    if (i != A.size() - 1 && A[i] == A[i + 1])
+    {
+      ans = "YES";
+    }
+  }
+
+  cout << ans << endl;
+}
