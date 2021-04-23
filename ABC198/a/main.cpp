f2fbb13ef394903f9e48bac0b71656a5cb611d02
Your Name <you@example.com>
2021-04-24 08:47:42 +0900

    ABC198/a

---
+++ ABC198/a/main.cpp
@@ -0,0 +1,12 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  cout << N - 1 << endl;
+}
